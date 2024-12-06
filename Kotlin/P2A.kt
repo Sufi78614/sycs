9613a76634267c40679919796a9c2829fe7fd381
Sufi78614 <95559750+Sufi78614@users.noreply.github.com>
2024-12-06 18:59:05 +0530

    Add files via upload

---
+++ Kotlin/P2A.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Keriwala Abdullah 5021")
+    println("=====================")
+    println("Enter the number:")
+    var num = Integer.parseInt(readLine())
+   
+        if (num % 2 == 0) {
+            println("$num is an Even Number")
+        } else {
+            println("$num is an Odd Number")
+        } 
+
+    println("Keriwala Abdullah 5021")
+}

9db7f4564901c58516b3f08d16e1c6eb055faebc
Sufi78614 <95559750+Sufi78614@users.noreply.github.com>
2024-12-13 23:48:12 +0530

    Add files via upload

---
+++ Kotlin/PRACTICAL 1/P1D.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+            println("Sufyan Ansari 5008")
+	println("===========================================")
+	println("Enter the two numbers : ")
+	var n1 = Integer.parseInt(readLine())
+	var n2 = Integer.parseInt(readLine())
+             val large=if(n1 > n2)
+	{
+               "$n1 Is Greater Than $n2"
+                }
+	else
+	{
+	"$n2 Is Greater Than $n1"
+	 }
+	println(large)
+	println("Sufyan Ansari 5008")
+}
---
+++ Kotlin/PRACTICAL 1/p1c.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+	println("Sufyan Ansari 5008")
+	println("Enter the Number")
+
+	var num1 = Integer.parseInt(readLine())
+	var num2 = Integer.parseInt(readLine())
+	var num3 = Integer.parseInt(readLine())
+	var num4 = Integer.parseInt(readLine())
+
+	var a = (num1 + num2 + num3 + num4)/4
+
+	println("the Number 1 is : $num1")
+	println("the Number 2 is : $num2")
+	println("the Number 3 is : $num3")
+	println("the Number 4 is : $num4")
+	println("the Average is : $a")
+	println("Sufyan Ansari 5008")
+}

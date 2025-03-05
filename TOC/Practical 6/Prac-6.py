0e4eae885b2e521725b4a9e24d67fe06a67e8228
Sufi78614 <95559750+Sufi78614@users.noreply.github.com>
2025-03-05 18:18:18 +0530

    Create Prac-6.py

---
+++ TOC/Practical 6/Prac-6.py
@@ -0,0 +1,28 @@
+import re
+print("Sufyan Ansari 5008")
+def generate_regex(grammar):
+    regex_dict = {}
+
+    for production in grammar:
+        non_terminal, expression = production.split(' -> ')
+        regex_dict[non_terminal] = expression
+
+    def replace_non_terminals(match):
+        non_terminal = match.group(1)
+        return '(' + regex_dict[non_terminal] + ')'
+
+    for non_terminal, expression in regex_dict.items():
+        regex_dict[non_terminal] = re.sub(r'([A-Z]+)', replace_non_terminals, expression)
+
+    final_regex = '^' + regex_dict['S'] + '$'
+    return final_regex
+
+# Example usage
+grammar = [
+    'S -> aA | bB',
+    'A -> aA | bB | ε',
+    'B -> bB | ε'
+]
+
+regex = generate_regex(grammar)
+print("Regular Expression:", regex)
